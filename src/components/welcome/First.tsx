d9fac8e078c70f4b43b1a08e890cafe0c06fe3d9
Dengky00 <1605980369@qq.com>
2023-07-11 20:47:38 +0800

    第一个欢迎界面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,8 +1,22 @@
 import { defineComponent } from 'vue';
+import style from './First.module.scss'
+import money from '../../assets/icons/money.svg'
+import { RouterLink } from 'vue-router';
+
 export const First = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div>First</div>
+            <div class={style.wrapper}>
+                <div class={style.card}>
+                    <img src={money} />
+                    <h2>会挣钱<br />还要会省钱</h2>
+                </div>
+                <div class={style.actions}>
+                    <RouterLink to="">跳过</RouterLink>
+                    <RouterLink to="/welcome/2">下一页</RouterLink>
+                    <RouterLink to="/start">跳过</RouterLink>
+                </div>
+            </div>
         )
     }
 })

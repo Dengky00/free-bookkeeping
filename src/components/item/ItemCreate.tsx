f740e7f251fc269b0d24aefae75198bb245cd5dd
Dengky00 <1605980369@qq.com>
2023-07-19 00:28:57 +0800

    完成初步的Tabs组件切换功能

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,20 +1,30 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import style from './ItemCreate.module.scss';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 
 export const ItemCreate = defineComponent({
     props: {
         name: {
             type: String as PropType<string>
-        }
+        },
     },
     setup: (props, context) => {
+        const kind = ref('支出')
         return () => (
             <MainLayout class={style.wrapper}>
                 {{
                     title: () => '记一笔',
-                    icon: () => <Icon name='left' />
+                    icon: () => <Icon name='left' />,
+                    default: () =>
+                        <>
+                            {/* <Tabs selected={kind.value} onUpdateSelected={name => kind.value = name}> */}
+                            <Tabs v-model:selected={kind.value}>
+                                <Tab name="支出">icon列表1</Tab>
+                                <Tab name="收入">icon列表2</Tab>
+                            </Tabs>
+                        </>
                 }}
             </MainLayout>
         )

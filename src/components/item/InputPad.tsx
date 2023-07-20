fbaed3d7ff98ec01b2af1bd872bfbfcc88bd2ec1
Dengky00 <1605980369@qq.com>
2023-07-20 21:57:49 +0800

    引入vantUI用于选择日期

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,24 +1,44 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import style from './InputPad.module.scss';
 import { Icon } from '../../shared/Icon';
+import dayjs from 'dayjs'
+import { DatePicker, Popup } from 'vant';
+import 'vant/lib/index.css';
 
 export const InputPad = defineComponent({
     props: {
-        amount: {
+        name: {
             type: String as PropType<string>,
-            default: '123456789.23',
+            default: '',
         }
     },
     setup: (props, context) => {
+        const refAmount = ref('')
+        const now = new Date()
+        const refDate = ref([dayjs(now).format('YYYY'), dayjs(now).format('MM'), dayjs(now).format('DD')])
+        const vantDate = ref(refDate.value)
+        const refDatePickerVisible = ref(false)
+        const showDatePicker = () => refDatePickerVisible.value = true
+        const hideDatePicker = () => refDatePickerVisible.value = false
+
         return () => (
             <div class={style.numberPad}>
                 <div class={style.dateAndAmount}>
-                    <div class={style.date}>
-                        <Icon name="date" class={style.icon} />
-                        <input type="date" />
+                    <div>
+                        <span class={style.date} onClick={showDatePicker}>
+                            <Icon name="date" class={style.icon} />
+                            {refDate.value[0] + '-' + refDate.value[1] + '-' + refDate.value[2]}
+                        </span>
+                        <Popup position='bottom'
+                            v-model:show={refDatePickerVisible.value}
+                            onClickOverlay={() => { vantDate.value = refDate.value; }}>
+                            <DatePicker v-model={vantDate.value} title='选择日期'
+                                onConfirm={() => { refDate.value = vantDate.value; hideDatePicker() }}
+                                onCancel={() => { vantDate.value = refDate.value; hideDatePicker() }} />
+                        </Popup>
                     </div>
-                    <div class={[style.amount, props.amount === '' ? style.empty : '']}>
-                        <div>{props.amount}</div>
+                    <div class={[style.amount, refAmount.value === '' ? style.empty : '']}>
+                        <div>{refAmount.value}</div>
                     </div>
                 </div>
 
@@ -26,24 +46,20 @@ export const InputPad = defineComponent({
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
-                    <button>+</button>
-                    {/* <button class={style.date}>
-                        <Icon name="date" class={style.icon} />
-                        时间
-                    </button> */}
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>-</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button class={style.ok}>OK</button>
-                    <button class={style.zero}>0</button>
-                    <button class={style.point}>.</button>
                     <button class={style.delete}>
                         <Icon name="backspace" class={style.icon} />
                     </button>
+                    {/* <button>+</button> */}
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    {/* <button>-</button> */}
+                    <button class={style.ok}>OK</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button class={style.zero}>0</button>
+                    <button class={style.point}>.</button>
                 </div>
             </div>
         )

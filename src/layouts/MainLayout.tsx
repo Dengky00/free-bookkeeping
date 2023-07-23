e660fa09ef7aa9267376241eab0bd06aa862ddc9
Dengky00 <1605980369@qq.com>
2023-07-24 00:52:39 +0800

    完成ItemCreate记一笔页面样式

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,10 @@
 import { defineComponent, ref } from 'vue';
-import style from './MainLayout.module.scss';
 import { NavBar } from '../shared/NavBar';
 
 export const MainLayout = defineComponent({
     setup: (props, context) => {
         return () => (
-            <div class={style.wrapper}>
+            <div>
                 <NavBar>
                     {{
                         icon: () => context.slots.icon?.(),

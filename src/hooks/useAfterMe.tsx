32ffeafe1a08b9efd52f9d002a2956dae42805cb
Dengky00 <1605980369@qq.com>
2023-08-17 13:35:18 +0800

    解决Vue关于 onMounted中有未处理的error的警告

--- src/hooks/useAfterMe.tsx
+++ src/hooks/useAfterMe.tsx
@@ -3,8 +3,7 @@ import { useMeStore } from '../stores/useMeStore'
 //登录后再执行数据请求
 export const useAfterMe = (fn: () => void) => {
   const meStore = useMeStore()
-  onMounted(async () => {
-    await meStore.mePromise
-    fn()
+  onMounted(() => {
+    meStore.mePromise!.then(fn, () => undefined)
   })
 }
